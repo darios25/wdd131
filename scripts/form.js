34da84bfaf0c984c464797d3b2b90d4fb02d8245
darios25 <dgiaccone@byupathway.edu>
2025-06-05 15:15:00 +0200

    1

---
+++ scripts/form.js
@@ -0,0 +1,24 @@
+
+const products = [
+    { id: "fc-1888", name: "flux capacitor" },
+    { id: "fc-2050", name: "power laces" },
+    { id: "fs-1987", name: "time circuits" },
+    { id: "ac-2000", name: "low voltage reactor" },
+    { id: "jj-1969", name: "warp equalizer" }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const productSelect = document.getElementById("product");
+
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+
+    // Contatore recensioni con localStorage
+    const reviewCounter = localStorage.getItem("reviewCount") || 0;
+    localStorage.setItem("reviewCount", Number(reviewCounter) + 1);
+});
+
